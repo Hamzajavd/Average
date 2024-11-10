55547a8d2df33543b2e4edd4accab9f5c77cf5e4
Hamza Javd <javedhamza895@gmail.com>
2024-11-10 14:41:03 +0500

    first commit

---
+++ Function.js
@@ -0,0 +1,22 @@
+// function add(num1, num2) {
+//     let addition = num1 + num2;
+//     return addition;
+//   }
+
+//   console.log(add(5, 9));
+
+
+function average (num){
+    let totalSum = 0;
+
+for(i=0; i < num.length; i++){
+    totalSum += num[i];
+}
+
+let AverageFormula = totalSum/num.length;
+return AverageFormula;
+
+}
+
+let array = [1,2,3,4,5,6,7,8]
+console.log(average(array));
